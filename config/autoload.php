e135a150881d58300536591d5c46cca1caef394b
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-05-01 15:26:50 +0800

    add autoload file
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+# Load the textmagic library when the spark is loaded
+$autoload['libraries'] = array('textmagic');
+
+# Load the textmagic config when the spark is loaded
+$autoload['config'] = array('textmagic');
